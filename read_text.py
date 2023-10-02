37338731771a0a668cd01f5b50e140ac21518c79
Jalen-coding <131199041+Jalen-coding@users.noreply.github.com>
2023-10-02 09:43:30 -0400

    Create read_text.py

---
+++ read_text.py
@@ -0,0 +1,29 @@
+file=input('What file would you like for me to open: ')
+
+sent=[]
+try:
+    with open("/home/judi/Documents/programming/text/"+file) as f:
+        sent=[word.rstrip() for word in f]
+
+except FileNotFoundError:
+    print('file does not exist')
+    exit()
+
+words=[]
+for s in sent:
+    s=s.split(' ')
+    words.append(s)
+
+num_char=0
+num_words=0
+num_lines=0
+for i in words:
+    num_lines+=1
+    for j in i:
+        num_words+=1
+        for k in j:
+            num_char+=1
+
+print('Number of characters:',num_char)
+print('Number of words:',num_words)
+print('Number of lines:',num_lines)
